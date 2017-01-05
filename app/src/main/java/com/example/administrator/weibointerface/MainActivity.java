31c04aca2f6f856b2287bcf4da7141db131720c2
LOU TING <631657124@qq.com>
2017-01-05 18:09:01 +0800

    primary interface completed,can scroll down and pop back

--- app/src/main/java/com/example/administrator/weibointerface/MainActivity.java
+++ app/src/main/java/com/example/administrator/weibointerface/MainActivity.java
@@ -1,12 +1,65 @@
 package com.example.administrator.weibointerface;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import view.PullScrowView;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TableLayout listlayout;
+    private ImageView header;
+    private PullScrowView scrowView;
+    private ImageView icon;
+    private int [] icons={R.mipmap.p1calendar,R.mipmap.p2camera,R.mipmap.p3clock,R.mipmap.p4collection,
+    R.mipmap.p5costcontrol,R.mipmap.p6dialer,R.mipmap.p7email,R.mipmap.p8finddevice,
+    R.mipmap.p9fm,R.mipmap.p10gallery,R.mipmap.p11keyboard,R.mipmap.p12music,R.mipmap.p13ringtones,
+    R.mipmap.p14settings,R.mipmap.p15sms,R.mipmap.p16vertical,R.mipmap.p17video,
+    R.mipmap.p18wallpaper};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        listlayout=(TableLayout)findViewById(R.id.list);
+        header=(ImageView)findViewById(R.id.image);
+        scrowView=(PullScrowView)findViewById(R.id.scrollview);
+        icon=(ImageView)findViewById(R.id.icon);
+        scrowView.setHeaderView(header);
+        initData();
+    }
+
+    private void initData(){
+        TableRow.LayoutParams params1=new TableRow.LayoutParams(200,200);
+        params1.setMargins(8,8,8,8);
+        params1.gravity=Gravity.LEFT;
+        for(int i=0;i<18;i++){
+            TableRow row=new TableRow(this);
+            ImageView imageView=new ImageView(this);
+            imageView.setImageResource(icons[i]);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            row.addView(imageView,params1);
+            TableRow.LayoutParams params2=new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params2.weight=1;
+            TextView textView=new TextView(this);
+            textView.setText("This is the item "+i+", click to check out more.");
+            params2.gravity=Gravity.CENTER_VERTICAL;
+            textView.setTextSize(16);
+            row.addView(textView,params2);
+            listlayout.addView(row);
+        }
+        header.setImageResource(R.drawable.cat);
+        icon.setImageResource(R.mipmap.icon);
     }
 }
--- app/src/main/java/view/PullScrowView.java
+++ app/src/main/java/view/PullScrowView.java
@@ -5,30 +5,29 @@ import android.content.res.TypedArray;
 import android.graphics.PointF;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ScrollView;
 
 import com.example.administrator.weibointerface.R;
+
+import static com.example.administrator.weibointerface.R.attr.header;
 
 /**
  * Created by Administrator on 2016/12/16.
  */
 
 public class PullScrowView extends ScrollView {
-    //private static final String LOG_TAG="PullScrowView";
-    //private static final float SCROLL_RATIO=0.5f;//阻尼系数，值越小阻尼就越大
-    //private static final int TURN_DISTANCE=100;//滑动至翻滚的距离
-    //private View mHeader;//头部的View
+
     private int headerHeight;//头部View的高度
     private int headerVisibleHeight;//头部view显示高度
-    //private View mContentView;//ScrollView的content view
-    //private Rect mContentRect=new Rect();//ScrollView的content view矩形
-    //private PointF mStartPoint=new PointF();//首次点击的Y坐标
-    //private boolean isMoving=false;//是否开始移动
-    //private boolean isTop=false;//是否移动到顶部位置
-    //private int mInitTop,mInitBottom;//头部图片初始顶部和底部
-    //private int mCurrentTop,mCurrentBottom;//头部图片拖动时顶部和底部
-    //private OnTurnListener mOnTurnListener;
+    private View contentView;//ScrollView的content view
+    private View headerView;
+    private float downY;
+    private int originalTop,originalBottom;
+    private final float fraction=0.5f;
+
     /*private enum State{
         UP,//顶部
         DOWN,//底部
@@ -39,18 +38,18 @@ public class PullScrowView extends ScrollView {
     //三种构造方法
     public PullScrowView(Context context){
         super(context);
-        init(context,null);
+        //init(context,null);
     }
     public PullScrowView(Context context, AttributeSet attrs){
         super(context,attrs);
-        init(context,attrs);
+        //init(context,attrs);
     }
     public PullScrowView(Context context,AttributeSet attrs,int defStyle){
         super(context,attrs,defStyle);
-        init(context,attrs);
+        //init(context,attrs);
     }
 
-    private void init(Context context,AttributeSet attrs){
+    /*private void init(Context context,AttributeSet attrs){
         setOverScrollMode(OVER_SCROLL_NEVER);//设置不能滑动过界
         if(attrs!=null){
             TypedArray array=context.obtainStyledAttributes(attrs,R.styleable.scrow);
@@ -58,5 +57,45 @@ public class PullScrowView extends ScrollView {
             headerVisibleHeight=(int)array.getDimension(R.styleable.scrow_headerVisibleHeight,0);
             array.recycle();
         }
+    }*/
+
+    public void setHeaderView(View view){
+        headerView=view;
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        if(getChildCount()>0)
+            contentView=getChildAt(0);
+    }
+
+    private int movement;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                downY=event.getY();
+                Log.e("downY",downY+"");
+                originalTop=headerView.getTop();
+                originalBottom=headerView.getBottom();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float curY=event.getY();
+                Log.e("curY",curY+"");
+                movement=(int)((curY-downY)*fraction);
+                if((curY-downY)>getScrollY()){
+                    movement-=getScrollY();
+                    if(movement>100)
+                        movement=100;
+                    if(movement>0)
+                        headerView.layout(contentView.getLeft(),originalTop+movement,contentView.getRight(),originalBottom+movement);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                headerView.layout(headerView.getLeft(),originalTop,headerView.getRight(),originalBottom);
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 }
